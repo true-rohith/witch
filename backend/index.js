90494e86f13af2266ec81a68c193e2248223b371
Rohith M Kokane <rkokane499@gmail.com>
2024-11-01 18:08:30 +0530

    url change from localhost to deployed frontend url

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://witch-rho.vercel.app", credentials: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
