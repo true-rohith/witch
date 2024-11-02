58920f94065a6723be01f3d71451bd6e2b2e1e7e
Rohith M Kokane <rkokane499@gmail.com>
2024-11-03 00:02:14 +0530

    Trying to solve DB Connection error

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -6,15 +6,15 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE;
 
-const connection = mysql.createConnection({
-  host: host,
-  user: user,
-  // port: 3307,
-  password: password,
-  database: database,
-});
-
+let connection;
 function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: host,
+    user: user,
+    // port: 3307,
+    password: password,
+    database: database,
+  });
   connection.connect((err) => {
     if (err) {
       console.error("Error connecting to the database:", err);
